e6e9122c9a268cad079e53c6526a21c192ca0341
asionbo <asionbo@126.com>
2021-07-23 16:56:49 +0800

    style: format code with dartfmt

--- lib/step/step_model.dart
+++ lib/step/step_model.dart
@@ -1,14 +1,11 @@
-
 import 'package:flutter/material.dart';
 
-class StepModel{
-
+class StepModel {
   /// title or header
   Widget title;
 
   /// container's content you want to show
   Widget content;
 
-
-  StepModel({this.title,this.content});
+  StepModel({this.title, this.content});
 }
